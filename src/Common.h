340d56a97941a55ed55c2b76ab13965a7eb6007b
lukaasm <lukaasm@gmail.com>
2012-03-21 12:07:27 +0100

    add glew and glfw(maybe will be replaced with glow)

---
+++ src/Common.h
@@ -0,0 +1,21 @@
+/*#################################################
+# Copyright (C) 2012 lukaasm <http://lukaasm.eu/>
+#################################################*/
+
+#ifndef H_COMMON
+#define H_COMMON
+
+typedef signed char     int8;
+typedef signed short    int16;
+typedef signed int      int32;
+typedef signed long     int64;
+
+typedef unsigned char   uint8;
+typedef unsigned short  uint16;
+typedef unsigned int    uint32;
+typedef unsigned long   uint64;
+
+#define delete_ptr(p) { delete p; p = NULL; }
+#define delete_array(p) { delete [] p; p = NULL; }
+
+#endif
