6beabebd627f7d5544eeb5e7c0f347d89d843660
lukaasm <lukaasm@localhost>
2012-03-23 23:56:02 +0100

    add callback on window close

--- src/CallBack.h
+++ src/CallBack.h
@@ -1,11 +1,11 @@
 /*#################################################
-# Copyright (C) 2012 lukaasm <http://lukaasm.eu/>
+# Copyright (C) 2006 Arash Partow <http://partow.net/>
 #################################################*/
 
 #ifndef H_CALLBACK
 #define H_CALLBACK
 
-template < class Class, typename ReturnType, typename Parameter1, typename Parameter2 >
+template < class Class, typename ReturnType, typename Parameter1 = void, typename Parameter2 = void>
 class CallBack
 {
     public:
@@ -20,6 +20,50 @@ class CallBack
         ReturnType operator()(Parameter1 p1, Parameter2 p2)
         {
             return (_instance->*_method)(p1, p2);
+        };
+
+    private:
+        Class* _instance;
+        Method  _method;
+};
+
+template < class Class, typename ReturnType, typename Parameter1>
+class CallBack < Class, ReturnType, Parameter1 >
+{
+    public:
+        typedef ReturnType (Class::*Method)(Parameter1);
+
+        CallBack(Class* instance, Method method)
+        {
+            _method = method;
+            _instance = instance;
+        };
+
+        ReturnType operator()(Parameter1 p1)
+        {
+           return (_instance->*_method)(p1);
+        };
+
+    private:
+        Class* _instance;
+        Method  _method;
+};
+
+template < class Class, typename ReturnType>
+class CallBack < Class, ReturnType >
+{
+    public:
+        typedef ReturnType (Class::*Method)();
+
+        CallBack(Class* instance, Method method)
+        {
+            _method = method;
+            _instance = instance;
+        };
+
+        ReturnType operator()()
+        {
+            return (_instance->*_method)();
         };
 
     private:
---
+++ src/Camera.h
@@ -0,0 +1,13 @@
+/*#################################################
+# Copyright (C) 2012 lukaasm <http://lukaasm.eu/>
+#################################################*/
+
+#ifndef H_CAMERA
+#define H_CAMERA
+
+class Camera
+{
+
+};
+
+#endif
