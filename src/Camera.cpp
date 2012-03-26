da8da5855aaddf8288ddbc2418be3592896b8a68
lukaasm <lukaasm@localhost>
2012-03-26 21:58:12 +0200

    Camera fixes :p rotating around _up vector done
    w,s,a,d movement q,e -> rotate around axis

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -7,6 +7,7 @@
 #include <GL/glfw.h>
 #include <glm/core/func_geometric.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/vector_angle.hpp>
 
 #include "RenderDevice.h"
 
@@ -16,10 +17,8 @@ Camera::Camera(RenderDevice* renderDevice)
 {
     _renderDevice = renderDevice;
 
-    _position = vec3(2.0f, 1.0f, 0.0f);
-    _lookAt = vec3(2.0f, 0.0f, -5.0f);
-
-    _angle = 0.0f;
+    _position = vec3(2.0f, 0.5f, 0.0f);
+    _lookAt = vec3(2.0f, 0.5f, -5.0f);
 
     _up = vec3(0.0f, 1.0f, 0.0f);
     _right = vec3(1.0f, 0.0f, 0.0f);
@@ -28,7 +27,6 @@ Camera::Camera(RenderDevice* renderDevice)
 void Camera::LookAt()
 {
     _viewMatrix = lookAt(_position, _lookAt, _up);
-    //_viewMatrix = translate(_viewMatrix, vec3(_position*-1.0f));
 }
 
 void Camera::OnResize()
@@ -55,21 +53,21 @@ void Camera::Move(MoveType type, float angleOrDist)
             break;
         }
         case MOVE_ROTATE_LEFT:
-            _angle += 0.005f;
+            RotateY(3.5f);
             break;
         case MOVE_ROTATE_RIGHT:
-            _angle -= 0.005f;
+            RotateY(-3.5f);
             break;
         case MOVE_STRAFE_RIGHT:
         {
-            _position -= _right*0.5f;
-            _lookAt -= _right*0.5f;
+            _position += normalize(_right);
+            _lookAt += normalize(_right);
             break;
         }
         case MOVE_STRAFE_LEFT:
         {
-            _position += _right*0.5f;
-            _lookAt += _right*0.5f;
+            _position -= normalize(_right);
+            _lookAt -= normalize(_right);
             break;
         }
     }
@@ -77,18 +75,16 @@ void Camera::Move(MoveType type, float angleOrDist)
 
 void Camera::RotateX(float angle)
 {
-//    _direction = normalize(rotateX(_up, angle));
-//    _up = cross(_direction, _right);
+    //_direction = rotateX(_up, angle));
+    //_up = cross(_direction, _right);
 }
 
 void Camera::RotateY(float angle)
 {
-  //  _direction = normalize(rotateY(_right, angle));
-  //  _right = cross(_direction, _up);
+    _lookAt = rotate(_lookAt, angle, _up);
+    _right = cross(_lookAt, _up);
 }
 
 void Camera::OnUpdate(const uint32 diff)
 {
-    //_position += _up*0.005f;
-    //_lookAt += _up*0.005f;
 }
