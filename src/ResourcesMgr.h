a1c9ec26707d5b156fd930d6aac0a8add3a5b7dd
lukaasm <lukaasm@gmail.com>
2012-10-25 15:55:19 +0200

    fix grid rendering, add basic ResourcesMGr.cpp

--- src/ResourcesMgr.h
+++ src/ResourcesMgr.h
@@ -13,7 +13,16 @@ class ResourcesMgr
     INIT_SINGLETON(ResourcesMgr)
 
     public:
+        void OnInit();
 
+    private:
+        void loadTextures() {}
+        void loadModels() {}
+        void loadShaders() {}
+
+        //uint32 GetTexture(std::string);
+        //uint32 GetModel(std::string);
+        //uint32 GetShader(std::string);
 };
 
 #define sResourcesMgr Singleton<ResourcesMgr>::Instance()
